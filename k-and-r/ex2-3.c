bed81a4f4ad329434466526f23cbae38d06d3929
yowcow <yowcow@cpan.org>
2016-05-04 19:11:50 +0900

    ex2-3.c

--- k-and-r/ex2-3.c
+++ k-and-r/ex2-3.c
@@ -39,15 +39,19 @@ int main() {
         char* a1 = "1234";
         char* a2 = "0123";
 
-        printf("atoi: %s => %d\n", a1, atoi(a1));
-        printf("atoi: %s => %d\n", a2, atoi(a2));
+        printf("atoi: %5s => %5d\n", a1, atoi(a1));
+        printf("atoi: %5s => %5d\n", a2, atoi(a2));
     }
 
     {
         char* h1 = "FF";
         char* h2 = "ff";
+        char* h3 = "0XFF";
+        char* h4 = "0xff";
 
-        printf("htoi: %s => %d\n", h1, htoi(h1));
-        printf("htoi: %s => %d\n", h2, htoi(h2));
+        printf("htoi: %5s => %5d\n", h1, htoi(h1));
+        printf("htoi: %5s => %5d\n", h2, htoi(h2));
+        printf("htoi: %5s => %5d\n", h3, htoi(h3));
+        printf("htoi: %5s => %5d\n", h4, htoi(h4));
     }
 }
