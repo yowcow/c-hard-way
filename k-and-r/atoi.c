b42eb1f62f4e28ae399bebbda66b682960c5f9e1
yowcow <yowcow@cpan.org>
2016-05-04 18:06:25 +0900

    add htoi

--- k-and-r/atoi.c
+++ k-and-r/atoi.c
@@ -4,17 +4,58 @@ int atoi(char* s) {
     int i, n;
 
     n = 0;
-    for (i = 0; *(s + i) != 0; i++) {
+    for (i = 0; *(s + i) >= '0' && *(s + i) <= '9'; i++) {
         n = 10 * n + (*(s + i) - '0');
     }
 
     return n;
 }
 
-int main() {
-    char* s1 = "1234";
-    char* s2 = "0123";
+char uc(char c) {
 
-    printf("%s => %d\n", s1, atoi(s1));
-    printf("%s => %d\n", s2, atoi(s2));
+    if (c >= 'a' && c <= 'z') {
+        c = c - 'a' + 'A';
+    }
+
+    return c;
+}
+
+int htoi(char* s) {
+    int i, n;
+
+    n = 0;
+    for (i = 0; *(s + i) != 0; i++) {
+        char c = uc(*(s + i));
+        int val = 0;
+
+        if (c >= '0' && c <= '9') {
+            val = c - '0';
+        }
+        else if (c >= 'A' && c <= 'F') {
+            val = c - 'A' + 10;
+        }
+
+        n = 16 * n + val;
+    }
+
+    return n;
+}
+
+int main() {
+
+    {
+        char* a1 = "1234";
+        char* a2 = "0123";
+
+        printf("atoi: %s => %d\n", a1, atoi(a1));
+        printf("atoi: %s => %d\n", a2, atoi(a2));
+    }
+
+    {
+        char* h1 = "FF";
+        char* h2 = "ff";
+
+        printf("htoi: %s => %d\n", h1, htoi(h1));
+        printf("htoi: %s => %d\n", h2, htoi(h2));
+    }
 }
